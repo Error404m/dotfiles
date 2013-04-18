a242ea3c67f6e079bba364c19818406c75611c6e
Revath S Kumar <rsk@revathskumar.com>
2013-04-18 18:47:35 +0530

    added alias rm=trash-put

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias rc='rails console'
 alias myip="curl -s icanhazip.com"
 alias bw='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s'
 alias st3='~/sublime_text_3/sublime_text'
+alias rm='trash-put'
