a8c21e20be4855bf26afc9a33f9cca2d49c8370f
Revath S Kumar <rsk@revathskumar.com>
2014-02-27 18:05:21 +0530

    Github : fetch pull request

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,12 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+# Fetch a pull request from github to a branch
+# @param pull request id
+# @param new branch name
+# Eg : gpr 216 features
+#      will fetch pull request 216 to the branch 216-features
+gpr(){
+  git fetch upstream pull/$1/head:$1-$2
+}
