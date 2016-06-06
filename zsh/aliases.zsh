3111ef864d0006c900b8735a43f14241455b281a
Revath S Kumar <rsk@revathskumar.com>
2016-06-06 19:07:06 +0530

    Adding alias for npm and update alias for rm for mac

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,12 @@ alias rc='rails console'
 alias myip="curl -s icanhazip.com"
 alias bw='echo "scale=2; `curl  --progress-bar -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test10.zip -o test.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s'
 alias st='subl'
-alias rm='trash-put'
+alias rm='trash'
 alias p8="ping 8.8.8.8"
 alias ack=ack-grep
 alias clrh="echo > ~/.local/share/recently-used.xbel"
+
+alias nis="npm i --save"
+alias nisp="npm i --save --progress=false"
+alias nid="npm i --save-dev"
+alias nid="npm i --save-dev --progress=false"
